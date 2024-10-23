10f6f3f65b56202c16d4c395e7a39b7c38f2663e
zzGHzz <40787792+zzGHzz@users.noreply.github.com>
2024-10-23 12:23:40 +0800

    add func to set log level

--- comm/init.go
+++ comm/init.go
@@ -1,10 +1,25 @@
 package comm
 
-import "log/slog"
+import (
+	"errors"
+	"log/slog"
+)
 
-var logLvl slog.Level
+var logLvl = slog.LevelInfo
 
-func init() {
-	// logLvl = slog.LevelDebug
-	logLvl = slog.LevelInfo
+func SetLogLevel(lvl string) error {
+	switch lvl {
+	case "debug":
+		logLvl = slog.LevelDebug
+	case "info":
+		logLvl = slog.LevelInfo
+	case "warn":
+		logLvl = slog.LevelWarn
+	case "error":
+		logLvl = slog.LevelError
+	default:
+		return errors.New("invalid log level")
+	}
+
+	return nil
 }
