67169ed9658c54d21bfb295646d005933a1d8136
zzGHzz <40787792+zzGHzz@users.noreply.github.com>
2024-10-23 13:06:02 +0800

    add comments

--- comm/config.go
+++ comm/config.go
@@ -1,22 +1,33 @@
 package comm
 
 type Config struct {
-	Name    string
-	Address string // host:port
+	// name given to the node
+	Name string
 
+	// IP address, in the form of host:port, given the TLS server run by the node
+	Address string
+
+	// paths to the TLS certificate and key used to run a TLS server
 	ServerCert string
 	ServerKey  string
 
+	// paths to the TLS certificate and key used to connect to other nodes
 	ClientCert string
 	ClientKey  string
 
+	// path to the CA certificate used to generate the above certificates
 	CACert string
 
 	Peers []PeerConfig
 }
 
 type PeerConfig struct {
-	Name    string
+	// name given to the peer
+	Name string
+
+	// IP address, in the form of host:port, given the TLS server run by the peer
 	Address string // host:port
-	CACert  string
+
+	// path to the CA certificate used to authenticate the peer during TLS handshake
+	CACert string
 }
